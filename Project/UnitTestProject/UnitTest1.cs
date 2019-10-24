7acdc85d6b56d7902f3ed6d5b0f698841b5e9d35
josua430 <56880004+josua430@users.noreply.github.com>
2019-10-24 08:08:35 -0500

    Test project
    Test project

---
+++ Project/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blog.Helpers;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// test classes for Posts
+    /// </summary>
+    [TestClass]
+    public class UnitTestPosts
+    {
+        /// <summary>
+        /// Not Exists post id
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_NotExistPost()
+        {
+            IDataBase objDataBase = new clsDataBaseMethodsPosts();
+
+            Assert.IsFalse(objDataBase.ExistsId("-1"));
+        }
+
+        /// <summary>
+        /// Exists post id
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_ExistPost()
+        {
+            IDataBase objDataBase = new clsDataBaseMethodsPosts();
+
+            Assert.IsTrue(objDataBase.ExistsId("1"));
+        }
+
+        /// <summary>
+        /// Not Exists post id
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_NotExistTitle()
+        {
+            IDataBase objDataBase = new clsDataBaseMethodsPosts();
+
+            Assert.IsFalse(objDataBase.ExistsTitle("-abcdeft76876889980"));
+        }
+
+
+    }
+}
